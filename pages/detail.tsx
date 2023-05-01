029488efab9659bcb8efae1a64eb9d9a92847ff4
kukizon0 <kukizonok@outlook.jp>
2023-05-01 12:14:40 +0900

    modify cssをまともにした

--- pages/detail.tsx
+++ pages/detail.tsx
@@ -1,21 +1,22 @@
 import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+// import styles from '@/styles/Home.module.css'
 import { Stint_Ultra_Condensed } from 'next/font/google'
 export default function Detail(props:any) {
 
-
+  const ptag = "font-normal text-gray-700 dark:text-gray-400 pb-7"
+  const h3tag = "font-bold h3tag "
     return (
-      <style tsx>{`
-        p {
-            display: grid;
-            place-items: center;
-        },
-        `}
-      </style> 
-        <div className='container'>
-          <div className=' mx-auto'>
+      // <style jsx>{`
+      //   p {
+      //       display: grid;
+      //       place-items: center;
+      //   },
+      //   `}
+      // </style> 
+        <div className='container mt-20 ml-10'>
+          <div className='text-center'>
             <Image
-              // className={styles.logo}
+              className='flex  mx-auto mt-10'
               src={props.book.book_img_link}
               alt="Next.js Logo"
               width={150}
@@ -23,26 +24,26 @@ export default function Detail(props:any) {
               // priority
             />
           
-              <h1 className='font-bold text-blue-800'>{props.book.book_title}</h1>
-              <p>{props.book.book_author}</p>
+              <h1 className='font-bold text-blue-800 text-4xl pt-6 pb-2'>{props.book.book_title}</h1>
+              <p className='font-normal text-gray-800 pb-8'>{props.book.book_author}</p>
            
           </div>
              
 
           <div>
-            <h3 className='text-l font-bold'>{props.book.headline1}</h3>
-            <p>{props.book.content1}</p>
+            <h3 className={h3tag}>{props.book.headline1}</h3>
+            <p className={ptag}>{props.book.content1}</p>
           </div>
           
           <div>
-          <h3 className='text-l font-bold'>{props.book.headline2}</h3>
-            <p className=' text-gray-350'>{props.book.content2}</p>
+          <h3 className={h3tag}>{props.book.headline2}</h3>
+            <p className={ptag}>{props.book.content2}</p>
           </div>
 
 
           <div>
-            <h3 className='text-l font-bold'>{props.book.detail_explanation_title}</h3>
-            <p className=' text-gray-400'>{props.detail_explanation_content}</p>
+            <h3 className={h3tag}>{props.book.detail_explanation_title}</h3>
+            <p className={ptag}>{props.book.detail_explanation_content}</p>
           </div>
         
         </div>
